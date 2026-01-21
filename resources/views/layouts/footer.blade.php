84f838581ecb3523778eabdc44ebaa6c062e75d5
fremasadi <fremasadii@gmail.com>
2026-01-21 20:17:41 +0700

    feat

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
         <div class="mb-2 mb-md-0">
             © <script>document.write(new Date().getFullYear())</script>
-            — <strong>UD WiNGKO LONDO</strong>
+            — <strong>UMKM WINGKO LONDO</strong>
         </div>
         <div>
             <a href="#" class="footer-link me-4">Lisensi</a>
